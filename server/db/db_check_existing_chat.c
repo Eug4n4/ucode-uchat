5f558d9201e42e45b008067bec2f14a509b04111
Ivan <151543441+IvanBardakov@users.noreply.github.com>
2024-11-09 11:46:13 +0200

    ivan: made db_check_existing_chat function not to check group chats

--- server/db/db_check_existing_chat.c
+++ server/db/db_check_existing_chat.c
@@ -4,8 +4,13 @@ int db_check_existing_chat(int user1_id, int user2_id) {
     sqlite3 *db = db_open("test.db");
     sqlite3_stmt *stmt;
     int chat_id = -1;
-    const char *sql = "SELECT chat_id FROM chat_users WHERE user_id IN (?, ?) GROUP BY chat_id HAVING COUNT(DISTINCT user_id) = 2;";
-    
+    const char *sql = "SELECT c.id "
+                      "FROM chat_users cu1 "
+                      "JOIN chat_users cu2 ON cu1.chat_id = cu2.chat_id "
+                      "JOIN chats c ON cu1.chat_id = c.id "
+                      "WHERE cu1.user_id = ? AND cu2.user_id = ? "
+                      "AND c.type = 0";
+
     if (sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) == SQLITE_OK) {
         sqlite3_bind_int(stmt, 1, user1_id);
         sqlite3_bind_int(stmt, 2, user2_id);
@@ -16,3 +21,4 @@ int db_check_existing_chat(int user1_id, int user2_id) {
     }
     return chat_id;
 }
+
